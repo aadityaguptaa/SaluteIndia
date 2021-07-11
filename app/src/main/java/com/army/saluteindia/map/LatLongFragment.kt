4716a05f1e966739a29ba49383514d56bdb4f231
Aditya Gupta <52538173+aadityaguptaa@users.noreply.github.com>
2021-07-11 19:17:38 +0530

    integrated google maps

---
+++ app/src/main/java/com/army/saluteindia/map/LatLongFragment.kt
@@ -0,0 +1,39 @@
+package com.army.saluteindia.map
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import com.army.saluteindia.R
+import com.army.saluteindia.databinding.FragmentLatLongBinding
+
+
+class LatLongFragment : Fragment() {
+
+    lateinit var binding: FragmentLatLongBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_lat_long, container, false
+        )
+
+
+        binding.mapButton.setOnClickListener { view: View ->
+            val latitude = binding.latitudeEditText.text.toString().toFloat()
+            val longitude = binding.longitudeEditText.text.toString().toFloat()
+
+            Navigation.findNavController(view).navigate(LatLongFragmentDirections.actionGoogleMapFragmentToMapsFragment(latitude = latitude, longitude = longitude))
+        }
+
+        return binding.root
+
+    }
+
+
+}
