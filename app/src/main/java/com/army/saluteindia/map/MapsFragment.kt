ea1c0160d09492c8c4e28e91e413693c1264fbd0
Aditya Gupta <52538173+aadityaguptaa@users.noreply.github.com>
2021-07-12 23:32:02 +0530

    map t-3

--- app/src/main/java/com/army/saluteindia/map/MapsFragment.kt
+++ app/src/main/java/com/army/saluteindia/map/MapsFragment.kt
@@ -1,24 +1,29 @@
 package com.army.saluteindia.map
 
-import androidx.fragment.app.Fragment
-
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.army.saluteindia.R
-
 import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
+
 class MapsFragment : Fragment() {
 
     private val args: MapsFragmentArgs by navArgs()
 
+
+
+    var latitude = 0.0
+    var longitude = 0.0
 
     private val callback = OnMapReadyCallback { googleMap ->
         /**
@@ -31,7 +36,17 @@ class MapsFragment : Fragment() {
          * user has installed Google Play services and returned to the app.
          */
         val sydney = LatLng(args.latitude.toDouble(), args.longitude.toDouble())
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        var mumbai = LatLng(19.0760, 72.8777)
+        val delhi = LatLng(28.6139, 77.2090)
+        var markerMumbai = MarkerOptions().position(mumbai).title("Marker in Sydney")
+        googleMap.addMarker(markerMumbai)
+        googleMap.addMarker(MarkerOptions().position(delhi).title("Marker in delhi"))
+        googleMap.setOnMarkerClickListener(OnMarkerClickListener { marker ->
+            Log.i("infofo", marker.title)
+            //Using position get Value from arraylist
+            false
+        })
+
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 
@@ -40,8 +55,11 @@ class MapsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         return inflater.inflate(R.layout.fragment_maps, container, false)
     }
+
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
